b55f300ecb115c50a552833cc79a1ec6bd905da8
Seggy Umboh <seggy.umboh@gmail.com>
2012-12-04 17:33:31 -0800

    Avoid overriding the good database/password

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ module.exports.createClient = module.exports.connect = function(redis_url) {
   var password = parsed_auth.join(':');
   var redis = require('redis').createClient(parsed_url.port, parsed_url.hostname);
 
-  if (password = parsed_auth[1]) {
+  if (password) {
     redis.auth(password, function(err) {
       if (err) throw err;
     });
   }
 
-  if (database = parsed_auth[0]) {
+  if (database) {
     redis.select(database);
     redis.on('connect', function() {
       redis.send_anyways = true
