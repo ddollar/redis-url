1007e81e37aee2c7f0af484967c8f659e05b9dae
Hunter Loftis <hunter@hunterloftis.com>
2014-10-10 14:55:35 -0700

    username should not be used to select database

--- index.js
+++ index.js
@@ -2,27 +2,27 @@ var url = require('url');
 var querystring = require('querystring');
 
 module.exports.createClient = module.exports.connect = function(redis_url) {
-  var password, database;
   var parsed_url  = url.parse(redis_url || process.env.REDIS_URL || 'redis://localhost:6379');
   var parsed_auth = (parsed_url.auth || '').split(':');
   var options = querystring.parse(parsed_url.query);
+  var password = parsed_auth[1];
+  var path = (parsed_url.pathname || '/').slice(1);
+  var database = path.length ? path : '0';
 
   var redis = require('redis').createClient(parsed_url.port, parsed_url.hostname, options);
 
-  if (password = parsed_auth[1]) {
+  if (password) {
     redis.auth(password, function(err) {
       if (err) throw err;
     });
   }
 
-  if (database = parsed_auth[0]) {
+  redis.select(database);
+  redis.on('connect', function() {
+    redis.send_anyways = true
     redis.select(database);
-    redis.on('connect', function() {
-      redis.send_anyways = true
-      redis.select(database);
-      redis.send_anyways = false;
-    });
-  }
+    redis.send_anyways = false;
+  });
 
   return(redis);
 }
