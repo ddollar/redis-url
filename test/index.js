94897ec8e80eca11a267f1d5ceb4112015ff84e7
Zeke Sikelianos <zeke@sikelianos.com>
2015-03-20 11:59:27 -0700

    add some tape tests

---
+++ test/index.js
@@ -0,0 +1,21 @@
+var test = require("tape")
+
+test("redis-url", function (t) {
+  t.plan(2);
+
+  var redis = require('..').createClient();
+  var redis2 = require('..').createClient('redis://localhost:6379');
+
+  redis.set('foo', 'bar');
+  redis.get('foo', function(err, res) {
+    if (err) throw errr
+    t.equal(res, 'bar', "foo should equal bar");
+  });
+
+  redis2.set('food', 'bar');
+  redis2.get('food', function(err, res) {
+    if (err) throw errr
+    t.equal(res, 'bar');
+  });
+
+})
